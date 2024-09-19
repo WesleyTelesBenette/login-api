b71b15c9facab72e598e1619eb236a2cbfeb376d
Wesley T. Benette <weluonf@gmail.com>
2024-09-19 08:53:12 -0300

    :wrench: chore: Renames folders to singular.

--- src/main/java/com/wesleytb/project_login/model/User.java
+++ src/main/java/com/wesleytb/project_login/model/User.java
@@ -1,4 +1,4 @@
-package com.wesleytb.project_login.models;
+package com.wesleytb.project_login.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
--- src/main/java/com/wesleytb/project_login/repository/UserRepository.java
+++ src/main/java/com/wesleytb/project_login/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.wesleytb.project_login.repositories;
+package com.wesleytb.project_login.repository;
 
-import com.wesleytb.project_login.models.User;
+import com.wesleytb.project_login.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
