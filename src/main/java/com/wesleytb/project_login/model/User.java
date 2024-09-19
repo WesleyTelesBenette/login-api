9d3e6c66bae7bbf5b443a01be50a750204a57562
Wesley T. Benette <weluonf@gmail.com>
2024-09-19 19:38:08 -0300

    :sparkles: feat(User): Update Model.
    - Writing error messages
    - Creating the IUser Interface

--- src/main/java/com/wesleytb/project_login/model/User.java
+++ src/main/java/com/wesleytb/project_login/model/User.java
@@ -1,22 +1,41 @@
 package com.wesleytb.project_login.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import com.wesleytb.project_login.service.interfaces.IUser;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 
 @Entity
 @Table(name = "login_app_user")
-public class User {
+public class User implements IUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String email;
-    String password;
+    private Long id;
+
+    @Length(min = 3, max = 255, message = "O nome deve ter entre 3 e 255 caracteres.")
+    @Column(name = "nome")
+    private String name;
+
+    @Range(min = 1, max = 150, message = "A idade deve ser entre 1 e 150 anos.")
+    @Column(name = "idade")
+    private Short age;
+
+    @Email(message = "Formato de e-mail incorreto.")
+    @Length(max = 255, message = "O e-mail deve ter no máximo 255 caracteres.")
+    @Column(name = "e-mail")
+    private String email;
+
+    @Length(min = 8, max = 50, message = "A senha deve ter entre 8 e 50 caracteres.")
+    @Column(name = "senha")
+    private String password;
 }
---
+++ src/main/java/com/wesleytb/project_login/service/interfaces/IUser.java
@@ -0,0 +1,16 @@
+package com.wesleytb.project_login.service.interfaces;
+
+public interface IUser {
+
+    String getName();
+    void setName(String name);
+
+    Short getAge();
+    void setAge(Short age);
+
+    String getEmail();
+    void setEmail(String email);
+
+    String getPassword();
+    void setPassword(String password);
+}
